20c6419faea6ef43c5a1aed49649fe93e933290b
Ivan <yzhang24@ualberta.ca>
2021-09-24 12:38:58 -0600

    update ass1

--- server.py
+++ server.py
@@ -26,30 +26,81 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-class HTTPRequestHandler():
-    def __init__(self, request):
-        """
-        This will parse the request into a request type and request path  
-        """
-        # Define our status code and phrases
-        self.status_code_phrase = {200: "OK", 301: "Moved Permanently", 403: "Forbidden", 404: "Not Found", 405: "Method Not Allowed"}
-
-        request = request.decode().split()
-        self.request_type = request[0]
-        self.request_path = request[1]
-    
-    def handle_request():
-        pass
-
-
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        http_response = HTTPRequestHandler(self.data).handle_request()
-        self.request.sendall(bytearray(http_response,'utf-8'))
+
+        request = self.data.decode().split()
+        request_type = request[0]
+        request_path = request[1]
+
+        # if the request type is not GET, response 405
+        if(request_type != "GET"):
+            self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed",'utf-8'))
+
+        else:
+            isPathComplete = 0
+            root = "www"
+            url = "http://127.0.0.1:8080" + request_path 
+            
+            if(request_path[-1] == '/'):
+                request_path = request_path + "index.html"
+            
+            request_split = request_path.split(".")
+       
+
+            # if curl path is like www/ab, we assume it's a folder
+
+            if(len(request_split) == 1):
+                request_path = request_path + "/index.html"
+                request_split.append("html")
+                url = url + '/'
+                isPathComplete = 1 # it's 1 if there's no '.' from input, we do 301
+
+            if(len(request_split) <= 2 and
+                (request_split[-1] == "css" or request_split[-1] == "html")):  
+
+
+                try:
+                    # try to return the content at the path with a 200 code
+                    with open(root+request_path, 'r') as file:
+    
+                        if (isPathComplete == 0):
+                            self.request.sendall(bytearray("HTTP/1.1 200 OK\r\n",'utf-8'))
+                        elif (isPathComplete == 1):
+                            # redirect if there no '/' from the input
+                            self.request.sendall(bytearray("HTTP/1.1 301 Moved Permanently\r\n",'utf-8'))
+                            self.request.sendall(bytearray("Location: " + url + "\r\n",'utf-8'))
+    
+                        if (request_split[-1] == "html"):
+                            content_type = "Content-Type: text/html\r\n"
+                        else:
+                            content_type = "Content-Type: text/css\r\n"
+
+                        self.request.sendall(bytearray(content_type,'utf-8'))
+                        
+                        l = file.read(1024)
+                        while (l):
+                            self.request.send( bytearray(l,'utf-8') )
+                            l = file.read(1024)
+                        file.close()
+
+                
+                except:
+                    self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
+
+                
+            else:
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
+
+        return 0
+
+
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
